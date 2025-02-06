30869802afaff3e6552287b13e17083d524aab4d
Fede <fede.atzeni372@gmail.com>
2025-02-06 17:22:39 +0100

    feat CRUD

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,21 +1,50 @@
-function index(req, res) { 
-    res.send("Lista dei post");
-}
+const posts = require("../data/posts");
 
-function show(req, res) { 
-    res.send(`Visione del post ${req.params.id}`);
-}
+// index
+function index(req, res) {
+    res.json(posts);
+};
 
-function create(req, res) { 
-    res.send("Creazione nuovo post")
-}
+//show
+function show(req, res) {
+    // res.send(`Visione del post ${req.params.id}`);
+    let post = posts.find((el) => el.id === parseInt(req.params.id));
 
-function update(req, res) { 
+    //per id inesistenti
+    if (!post) {
+        res.status(404);
+        res.send("not found");
+    }
+
+    res.json(post);
+};
+
+//create
+function create(req, res) {
+    res.send("Creazione nuovo post");
+};
+
+//update
+function update(req, res) {
     res.send(`Modifica del post ${req.params.id}`);
-}
+};
 
-function destroy(req, res) { 
-    res.send(`Eliminazione del post ${req.params.id}`)
-}
+//destroy
+function destroy(req, res) {
+    // res.send(`Eliminazione del post ${req.params.id}`);
+    let post = posts.find((el) => el.id === parseInt(req.params.id));
 
-module.exports = {index, show, create, update, destroy}
+    //per id inesistenti
+    if (!post) {
+        res.status(404);
+        res.send("not found");
+    }
+
+    posts.splice(posts.indexOf(post), 1);
+
+    res.sendStatus(204);
+    console.log(posts);
+};
+
+//export
+module.exports = { index, show, create, update, destroy };
